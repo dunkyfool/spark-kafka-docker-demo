98cad9688c805080d68d6d3c5d992f6f434669f1
dunkyfool <dunkyfool0113@gmail.com>
2016-10-18 12:03:55 +0800

    add toolkit image into docker-compose

--- enter.sh
+++ enter.sh
@@ -4,6 +4,8 @@ if [ "$1" = "spark" ];then
 	docker exec -it test_spark bash
 elif [ "$1" = "kafka" ];then
 	docker exec -it test_kafka bash
+elif [ "$1" = "toolkit" ];then
+	docker exec -it test_toolkit bash
 elif [ "$1" = "" ];then
-	echo "Usage ./enter.sh [kafka|spark]"
+	echo "Usage ./enter.sh [kafka|spark|toolkit]"
 fi
